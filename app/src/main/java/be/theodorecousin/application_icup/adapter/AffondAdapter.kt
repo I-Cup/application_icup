f86d80a6abea7aaac5beca72fd26cd532e6e4569
Alex_K-47 <kneipalexandre@gmail.com>
2021-12-01 20:25:54 +0100

    premiere esquisse vertical recycler

--- app/src/main/java/be/theodorecousin/application_icup/adapter/AffondAdapter.kt
+++ app/src/main/java/be/theodorecousin/application_icup/adapter/AffondAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import be.theodorecousin.application_icup.R
 
@@ -18,21 +19,21 @@ class AffondAdapter : RecyclerView.Adapter<AffondAdapter.ViewHolder>(){
 
         //box_item est une variable venant du fichier item_vertical_affond.xml
         //il s'agit de l'élément générique que nous allons injecter dans notre linear layout dynamic
-        val affondBox = view.findViewById<FrameLayout>(R.id.box_item)
+        val chronoIcon = view.findViewById<ImageView>(R.id.chrono_icon)
+
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater
             .from(parent.context)
-            .inflate(R.layout.item_vertical_affond, parent, false)
+            .inflate(R.layout.item_vertical_chronos, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {}
 
-    override fun getItemCount(): Int {
-        return 5
-    }
+    override fun getItemCount(): Int =24
+
 
 }
