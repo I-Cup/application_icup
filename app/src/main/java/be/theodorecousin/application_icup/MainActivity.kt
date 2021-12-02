f680655ab9d73357b6e56ecfaf7c81f0b7f27ed1
TheodoreCouz <theodore.cousin@student.uclouvain.be>
2021-12-02 19:21:14 +0100

    comments

--- app/src/main/java/be/theodorecousin/application_icup/MainActivity.kt
+++ app/src/main/java/be/theodorecousin/application_icup/MainActivity.kt
@@ -14,9 +14,17 @@ class MainActivity : AppCompatActivity() {
         //injecter le fragment dans fragment_container
 
         val transaction = supportFragmentManager.beginTransaction()
+
+        //injecte le linear layout affichant les chrono dans le container prévu à cet effet
         transaction.replace(R.id.chrono_container, HomeFragment())
+
+        //injecte la top bar dans le container prévu à cet effet
         transaction.replace(R.id.top_bar_container,TopFragment())
+
+        //injecte la task bar dans le container prévu à cet effet
         transaction.replace(R.id.bottom_bar_container,BottomFragment())
+
+        //valider les changements
         transaction.addToBackStack(null)
         transaction.commit()
 
